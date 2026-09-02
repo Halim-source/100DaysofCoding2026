e49f3bdae9d2cc70a75756d831e3455917b6d2ad
halim <abdhalym522@gmail.com>
2026-09-02 18:06:33 +0800

    Refactor Day0.java by removing unnecessary code
    Removed unused variables and cleaned up code.

--- Day0.java
+++ Day0.java
@@ -1,20 +1,10 @@
-class Dasar{
+public class Day0{
 
-  // hesder
   public static void main(String[] args) {
 
   
   System.out.println("hsja");
 
-    String jenisKelamin = "cewek";
-    String alamat = "majene";
-    String umur = "19";
-
-    System.out.println(alamat);
-    System.out.println(umur);
-    System.out.println(jenisKelamin);
-
-    
 
   }
 }
