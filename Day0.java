2b8feca9bfcb559e6289b674d822bfd738a432d7
halim <abdhalym522@gmail.com>
2026-09-02 18:02:03 +0800

    Update Day0.java

--- Day0.java
+++ Day0.java
@@ -1,10 +1,19 @@
-class {
+class Dasar{
 
   // hesder
   public static void main(String[] args) {
 
   
-System.
+  System.out.println("hsja");
+
+    String jenisKelamin = "cewek";
+    String alamat = "majene";
+    String umur = "19";
+
+    System.out.println(alamat);
+    System.out.println(umur);
+    System.out.println(jenisKelamin);
+
     
 
   }
