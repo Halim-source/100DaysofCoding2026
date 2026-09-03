14643e022aac10439bcfa009022fd2d809e607e2
halim <abdhalym522@gmail.com>
2026-09-03 14:38:32 +0800

    update Day2.java

--- Day2.java
+++ Day2.java
@@ -1,6 +1,19 @@
 public class Day2{
 
 public static void main(String[] args) {
-  
+
+          //println
+  System.out.println("=====BIODATA====");
+  System.out.println("NAMA    :ABD.HALIM");
+  System.out.println("ALAMAT  :MAJENE");
+
+          //print
+  System.out.print  ("ALUMNI  :");
+  System.out.print  ("MA DDI BARUGA");
+
+  System.out.println("");
+        //printf
+  System.out.printf ("UMUR    :%sTAHUN %n", 19);
+  System.out.printf ("TINGGI  :%sCM %n", 153);
 }
 }
