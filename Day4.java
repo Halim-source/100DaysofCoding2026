0c0bc79609530152798460eebf31749b4a69c76d
halim <abdhalym522@gmail.com>
2026-09-05 20:40:12 +0800

    update Day4.java

---
+++ Day4.java
@@ -0,0 +1,17 @@
+public class Day4{
+
+public static void main(String[] args) {
+
+byte kecil=125;
+short sedang=25000;
+int besar =1000_000_000;
+long sangatbanyak=1000000000;
+
+  System.out.println("byte:"+kecil);
+  System.out.println("short:"+sedang);
+  System.out.println("int:"+besar);
+  System.out.println("long:"+sangatbanyak);
+  
+}
+  
+}
