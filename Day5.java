e1b4086bed9590bc5fdd09f648e561fb21265eda
halim <abdhalym522@gmail.com>
2026-09-06 21:15:24 +0800

    update Day5.java

---
+++ Day5.java
@@ -0,0 +1,18 @@
+public class Day5{
+
+  public static void main(String[] args) {
+
+
+          //double di akhir tidak usah pake huruf f
+    
+        double tinggisaya=153.3;
+    System.out.println("tinggi saya :"+tinggisaya+"cm");
+         // diakhir harus pake f
+        float beratsaya=44.4f;
+    System.out.println("berat saya  :"+beratsaya+"kg");
+
+    
+  }
+
+  
+}
