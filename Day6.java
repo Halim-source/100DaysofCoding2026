e1d517d43110ade77d1681835863cb06907f5a5f
halim <abdhalym522@gmail.com>
2026-09-07 22:22:49 +0800

    update Day6.java

---
+++ Day6.java
@@ -0,0 +1,14 @@
+
+public class day6 {
+
+    public static void main(String[] args) {
+        char inisial = 'H'; // simpan satu karakter/simbol
+        
+        boolean benar =(7>3);
+        boolean salah =(1>3);
+      System.out.println(inisial);
+      System.out.println("7>3 :"+benar);
+      System.out.println("1>3 :"+salah);
+    
+    }
+}
