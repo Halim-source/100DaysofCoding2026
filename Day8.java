d5f2458f3be0b383992818f991eaf4d2f2c3b83a
halim <abdhalym522@gmail.com>
2026-09-09 15:17:35 +0800

    update Day8.java

---
+++ Day8.java
@@ -0,0 +1,32 @@
+public class Day8{
+
+public static void main(String[] args) {
+
+
+  //deklarasi hanya nama variabel
+  String jokowi;
+  int umur;
+
+  /* inisialisasi ketika sudah diberi
+   nila dgn tanda "="*/
+  jokowi="priasolo";
+  umur=30;
+  
+   //update cukup ketik variabel lalu ubah nilai
+   jokowi="presidenku";
+  umur=35;
+
+  
+  System.out.println(jokowi);
+  System.out.println(umur);
+
+  
+    
+  
+
+  
+
+  
+}
+  
+}
