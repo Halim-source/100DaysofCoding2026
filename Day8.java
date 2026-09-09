05d8285feb5e424bdc03b6d93260d65587aba944
halim <abdhalym522@gmail.com>
2026-09-09 16:08:52 +0800

    update Day8.java

--- Day8.java
+++ Day8.java
@@ -12,11 +12,17 @@ public static void main(String[] args) {
   jokowi="priasolo";
   umur=30;
   
+
+
+  System.out.println("before");
+  System.out.println(jokowi);
+  System.out.println(umur);
+  
    //update cukup ketik variabel lalu ubah nilai
    jokowi="presidenku";
   umur=35;
 
-  
+  System.out.println("after");
   System.out.println(jokowi);
   System.out.println(umur);
 
