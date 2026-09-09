adef06213fd85c874efaf8a1300fc0f29ec64123
halim <abdhalym522@gmail.com>
2026-09-09 15:51:47 +0800

    update Day4.java

--- Day4.java
+++ Day4.java
@@ -7,6 +7,14 @@ short sedang=25000;
 int besar =1000_000_000;
 long sangatbanyak=1000000000;
 
+  
+  /*4 tipe data bilangan bulat sama nilai minimum-maksimum:
+1. byte  : -128 sampai 127
+2. short: -32.768 sampai 32.767
+3. int     : -2.147.483.648 sampai 2147483647
+4. long  : -9  quintiliun sampai 9 quintiliun*/
+  
+
   System.out.println("byte:"+kecil);
   System.out.println("short:"+sedang);
   System.out.println("int:"+besar);
