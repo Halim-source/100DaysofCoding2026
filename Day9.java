9f4c48f7566ff10998435e4fd8399479bea7b127
halim <abdhalym522@gmail.com>
2026-09-10 20:35:18 +0800

    update Day9.java

---
+++ Day9.java
@@ -0,0 +1,14 @@
+public class Day9{
+public static void main(String[] args) {
+
+final int jumlahhariseminggu=7;
+
+  System.out.println("jumlah hari seminggu ada :"+jumlahhariseminggu);
+
+
+  
+}
+
+
+  
+}
