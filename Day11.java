071ef43a1eb09f01bf6e1e0840d1c0704377e991
halim <abdhalym522@gmail.com>
2026-09-11 23:07:12 +0800

    update Day11.java

---
+++ Day11.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Day11{
+
+  public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+ System.out.println("kesejahteraan telah mengikuti anda,ketik ya untuk melanjutkan");
+    String amin =input.nextLine();
+
+    System.out.println("oke,selamat sejahtera");
+  
+  }
+}
