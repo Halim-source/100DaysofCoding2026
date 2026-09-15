5be834450bae56ae8f8b1ad61993566c2ac49ea5
halim <abdhalym522@gmail.com>
2026-09-15 20:28:16 +0800

    update soal1.java

---
+++ soal1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class soal1{
+public static void main(String[] args) {
+
+  Scanner scan1 =new Scanner(System.in);
+
+  //soal1
+  System.out.print("masukkan nomor hp :");
+  String nope =scan1.next();
+
+  System.out.println(nope);
+  
+
+
+  
+}
+
+  
+}
