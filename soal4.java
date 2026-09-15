43b1843b04da5a8747be0ede571e79b65af1e4b3
halim <abdhalym522@gmail.com>
2026-09-15 22:45:53 +0800

    update soal4.java

---
+++ soal4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class soal4{
+
+public static void main(String[] args) {
+
+Scanner scan1 =new Scanner(System.in);
+
+  System.out.println("masukkan nama perguruan tinggi :");
+  String univ =scan1.next();
+  String univ1 =scan1.next();
+  String univ2 =scan1.next();
+  
+
+  System.out.print(univ + " " + univ1 + " " + univ2);
+
+  
+}
+
+
+  
+}
