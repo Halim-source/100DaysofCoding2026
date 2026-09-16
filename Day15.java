ebefc6a659a47d68be06e736cc07246da3e9af30
halim <abdhalym522@gmail.com>
2026-09-16 15:15:08 +0800

    update Day15.java

---
+++ Day15.java
@@ -0,0 +1,12 @@
+public class Day15{
+public static void main (String[] args){
+
+int angka1 =20;
+int angka2 =5;
+
+  System.out.println("angka1 × angka2 ="+angka1*angka2);
+  System.out.println("angka1 ÷ angka2 ="+angka1/angka2);
+  
+ }
+
+}
