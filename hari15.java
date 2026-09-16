052d23f18774dc5b5c3e556ce2987e1534486c50
halim <abdhalym522@gmail.com>
2026-09-16 14:39:03 +0800

    update hari15.java

---
+++ hari15.java
@@ -0,0 +1,10 @@
+public class Hari15 {
+    public static void main(String[] args) {
+        int angka1 = 20;
+        int angka2 = 4;
+        
+    
+        
+        System.out.println("angka1 × angka2 =" +(angka1*angka2));
+    }
+}
