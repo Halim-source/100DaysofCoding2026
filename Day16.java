5706dee153c2fd74402f21b3760cda9cd4c16071
halim <abdhalym522@gmail.com>
2026-09-17 17:31:25 +0800

    update Day16.java

---
+++ Day16.java
@@ -0,0 +1,9 @@
+public class Day16 {
+    public static void main(String[] args) {
+        int angka = 33;
+        int pembagi = 5;
+        int sisaBagi = angka % pembagi;
+        
+        System.out.println("Sisa bagi dari " + angka+  " dibagi " + pembagi + " adalah " + sisaBagi);
+    }
+}
