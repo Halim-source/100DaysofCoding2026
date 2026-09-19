59643bc98b7a317436780bc390cc3f1ee45e4dfc
halim <abdhalym522@gmail.com>
2026-09-19 21:11:30 +0800

    update Day18.java

---
+++ Day18.java
@@ -0,0 +1,24 @@
+public class Day18{
+
+public static void main (String[] args) {
+
+System.out.println("nilai awal 125(byte)");
+
+  byte satu =125;
+  short dua =satu;
+  int tiga =dua;
+  long empat =tiga;
+  float lima=empat;
+  double enam =lima;
+
+  
+  System.out.println("byte  ="+satu);
+  System.out.println("short ="+dua);
+  System.out.println("int   ="+tiga);
+  System.out.println("long  ="+empat);
+  System.out.println("float="+lima);
+  System.out.println("double ="+enam);
+}
+
+  
+}
