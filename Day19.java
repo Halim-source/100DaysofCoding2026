7f0ca1e5820416aac6d4b5437b10a3ac624045db
halim <abdhalym522@gmail.com>
2026-09-20 21:00:42 +0800

    update Day19.java

---
+++ Day19.java
@@ -0,0 +1,17 @@
+public class Day19{
+public static void main (String[] args) {
+
+double desimal =20.99;
+
+  int angka =(int)desimal;
+  
+
+  System.out.println(desimal);
+  System.out.println(angka);
+
+  
+}
+
+
+  
+}
