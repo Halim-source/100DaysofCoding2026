8534664e42d74801f2ef5e6534a4c104b633cfd4
halim <abdhalym522@gmail.com>
2026-09-21 22:10:28 +0800

    update Day20.java

---
+++ Day20.java
@@ -0,0 +1,22 @@
+public class Day20{
+public static void main(String[] args) {
+
+
+  int umur = 20;
+  String stumur = String.valueOf(umur);
+
+  double tinggi =153.12;
+  String sting =String.valueOf(tinggi);
+
+  boolean tampan =true;
+  String fakta =String.valueOf(tampan);
+
+  System.out.println("string umur = "+umur);
+  System.out.println("string tinggi = "+tinggi);
+  System.out.println("string saya tampan = "+tampan);
+  
+}
+
+
+  
+}
