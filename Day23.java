12b6be8e7625d9cbf37f02ff88d7931e2547f7df
halim <abdhalym522@gmail.com>
2026-09-24 20:50:21 +0800

    update Day23.java

---
+++ Day23.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Day23{
+public static void main(String[]args){
+
+Scanner sc =new Scanner(System.in);
+
+  System.out.print("masukkan nilai sisi persegi :");
+  double sisi =sc.nextDouble();
+  double luas =sisi*sisi;
+
+  System.out.printf("luas persegi = %.0f " ,luas);
+  
+}
+
+  
+
+}
