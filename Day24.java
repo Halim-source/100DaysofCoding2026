ef67c253bf2ce4b81a87053ea310c20319f520f3
halim <abdhalym522@gmail.com>
2026-09-25 22:00:50 +0800

    update Day24.java

---
+++ Day24.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Day24{
+public static void main(String[] args) {
+  Scanner sc =new Scanner(System.in);
+
+    System.out.print("masukkan panjang = ");
+int p =sc.nextInt();
+    System.out.print("masukkan lebar   = ");
+  int l =sc.nextInt();
+
+  System.out.println("luas persegi ="+p*l);
+
+
+
+  
+}
+
+  
+}
