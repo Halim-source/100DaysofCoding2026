95148f19f2188d88a1f00404a452533e04f27010
halim <abdhalym522@gmail.com>
2026-09-26 22:38:39 +0800

    update Day14.java

---
+++ Day14.java
@@ -0,0 +1,12 @@
+public class Day14{
+public static void main (String[] args){
+
+int angka1 =20;
+int angka2 =5;
+
+  System.out.println("angka1 + angka2 ="+(angka1+angka2));
+  System.out.println("angka1 - angka2 ="+(angka1-angka2));
+  
+ }
+
+}
