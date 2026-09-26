61f4c85029b4d4c1ff888e86b11f6d2e66f6512e
halim <abdhalym522@gmail.com>
2026-09-26 18:27:38 +0800

    update Day25.java

---
+++ Day25.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Day25{
+public static void main(String[]args){
+
+Scanner sc =new Scanner(System.in);
+System.out.print("masukkan angka r lingkaran =");
+double r =sc.nextDouble();
+System.out.println("rumus luas lingkaran 3.14×r×r");
+  double luas =3.14*r*r;
+System.out.print("luas lingkaran ="+luas);
+
+
+}
+
+}
